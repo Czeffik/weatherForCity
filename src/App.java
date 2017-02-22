cc0550bea51d396ad4f886f49565ec07fb7df2e8
Czeffik <trzewik93@gmail.com>
2017-02-22 16:02:18 +0100

    FINAL VERSION

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        
+
         Scanner input = new Scanner(System.in);
         System.out.println("Wprowadz nazwe miasta,dla którego chcesz odczytywać dane: ");
         new WorkingProgram(input.nextLine());
