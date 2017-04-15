be15cfbe2534f1136c85a22071df88a4ceea790c
Czeffik <trzewik93@gmail.com>
2017-04-15 13:15:15 +0200

    as

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,42 @@
+import javafx.util.Pair;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by t on 15.04.2017.
  */
 public class NewClass {
+    private Map<Integer, Map<Integer,Double>> allDifferences;
+
+    public Map<Integer, Map<Integer, Double>> getAllDifferences() {
+        return allDifferences;
+    }
+
+    public NewClass(String city){
+        Map<Pair<Integer, Double>, Map<Integer, Double>> mapToRead = new ReadingFromFile(city).getMapToRead();
+        allDifferences = new LinkedHashMap<>();
+
+        for (Pair keyMapToRead: mapToRead.keySet()) {
+            int pairKey = (int) keyMapToRead.getKey();
+            double pairValue = (double) keyMapToRead.getValue();
+            Map<Integer, Double> differenceTimeAndTemperature = new TreeMap<>();
+
+            for (Pair keyMapToRead2 : mapToRead.keySet()) {
+                int pairKey2 = (int) keyMapToRead2.getKey();
+
+                for (int mapKey : mapToRead.get(keyMapToRead2).keySet()) {
+                    if(pairKey==mapKey){
+                        int key = (pairKey-pairKey2)/3600; //3600 second = 1 hour
+                        double value = pairValue - mapToRead.get(keyMapToRead2).get(mapKey);
+                        differenceTimeAndTemperature.put(key, value);
+                    }
+                }
+            }
+            if (differenceTimeAndTemperature.size() > 0) {
+                allDifferences.put(pairKey, differenceTimeAndTemperature);
+            }
+        }
+    }
 }
